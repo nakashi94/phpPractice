f45fa4da47dbeeb67962de6293e0a7d1111aedeb
nakashi94 <n0904yBUNJOU@gmail.com>
2022-01-19 20:58:22 +0900

    first commit

---
+++ hello.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+
+<body>
+    <h1>phpでHello World!</h1>
+    <?php $greeting = "こんにちは、世界！"; ?>
+    <p><?php echo $greeting; ?></p>
+</body>
+
+</html>
